dff47ee1b3fb40d8c53219f5b876bf218949b410
Jaynikhar <jaynikhar61@gmail.com>
2026-01-08 17:09:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return ( 
+    <>
+      <h1>Welcome to Jay's Portfolio</h1>
+      <br></br><br></br>
+      <h2>comming soon!</h2>
+    </>
+  );
+}
+export default App;
